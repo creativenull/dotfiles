3fec88c7b4f3f47721d2c53c93512c26ce14562f
Arnold Chand <creativenull@outlook.com>
2021-02-09 12:22:48 -0400

    feat: update commands.lua

--- .config/nvim-nightly/lua/creativenull/commands.lua
+++ .config/nvim-nightly/lua/creativenull/commands.lua
@@ -1,5 +1,11 @@
 vim.cmd('command! Config edit $MYVIMRC')
+
+-- TODO
+-- Seems like this doesn't reload all the modules
+-- find another alternative
 vim.cmd('command! ConfigReload luafile $MYVIMRC')
+
+-- I can't release my shift key fast enough :')
 vim.cmd('command! -nargs=* W w')
 vim.cmd('command! -nargs=* Wq wq')
 vim.cmd('command! -nargs=* Q q')
