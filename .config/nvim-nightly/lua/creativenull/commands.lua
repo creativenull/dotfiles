864a94dc758b18a1b328ee551087cf774ca9e583
Arnold Chand <creativenull@outlook.com>
2021-02-14 12:40:42 -0400

    feat: add reload config function and md/json conceal toggle

--- .config/nvim-nightly/lua/creativenull/commands.lua
+++ .config/nvim-nightly/lua/creativenull/commands.lua
@@ -1,13 +1,12 @@
-vim.cmd('command! Config edit $MYVIMRC')
-
--- TODO
--- Seems like this doesn't reload all the modules
--- find another alternative
-vim.cmd('command! ConfigReload luafile $MYVIMRC')
+vim.cmd 'command! Config edit $MYVIMRC'
+vim.cmd [[command! ConfigReload lua require'creativenull.utils'.reload_config()]]
 
 -- I can't release my shift key fast enough :')
-vim.cmd('command! -nargs=* W w')
-vim.cmd('command! -nargs=* Wq wq')
-vim.cmd('command! -nargs=* Q q')
-vim.cmd('command! -nargs=* Qa qa')
-vim.cmd('command! -nargs=* QA qa')
+vim.cmd 'command! -nargs=* W w'
+vim.cmd 'command! -nargs=* Wq wq'
+vim.cmd 'command! -nargs=* Q q'
+vim.cmd 'command! -nargs=* Qa qa'
+vim.cmd 'command! -nargs=* QA qa'
+
+-- Toggle MD/Json quotes
+vim.cmd [[command! ToggleConceal lua require'creativenull.utils'.toggle_conceal()]]
--- .config/nvim-nightly/lua/creativenull/utils.lua
+++ .config/nvim-nightly/lua/creativenull/utils.lua
@@ -24,4 +24,26 @@ function M.vmap(input, output) map('v', input, output) end
 
 function M.tmap(input, output) map('t', input, output) end
 
+function M.reload_config()
+    for k, v in pairs(package.loaded) do
+        if string.match(k, "^creativenull") then
+            package.loaded[k] = nil
+        end
+    end
+
+    -- TODO
+    -- Find another way to re-source our init.lua
+    -- outside of vim.cmd
+    vim.cmd 'luafile $MYVIMRC'
+end
+
+-- Toggle Conceal for markdown, json, etc
+function M.toggle_conceal()
+    if vim.wo.conceallevel == 2 then
+        vim.wo.conceallevel = 0
+    else
+        vim.wo.conceallevel = 2
+    end
+end
+
 return M
