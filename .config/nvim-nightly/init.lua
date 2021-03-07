fa5e1c23c2cf1c19fb686254b5340c0de0691393
Arnold Chand <creativenull@outlook.com>
2021-03-06 23:04:09 -0400

    feat: formatting, move runtimepath to top

--- .config/nvim-nightly/init.lua
+++ .config/nvim-nightly/init.lua
@@ -1,20 +1,4 @@
 -- Why do this? https://dev.to/creativenull/installing-neovim-nightly-alongside-stable-10d0
--- Pack Path
-vim.cmd('set packpath-=~/.config/nvim')
-vim.cmd('set packpath-=~/.config/nvim/after')
-vim.cmd('set packpath-=~/.local/share/nvim/site')
-vim.cmd('set packpath-=~/.local/share/nvim/site/after')
-vim.cmd('set packpath-=/etc/xdg/nvim')
-vim.cmd('set packpath-=/etc/xdg/nvim/after')
-vim.cmd('set packpath-=/usr/local/share/nvim/site')
-vim.cmd('set packpath-=/usr/local/share/nvim/site/after')
-vim.cmd('set packpath-=/usr/share/nvim/site')
-vim.cmd('set packpath-=/usr/share/nvim/site/after')
-vim.cmd('set packpath^=~/.config/nvim-nightly')
-vim.cmd('set packpath+=~/.config/nvim-nightly/after')
-vim.cmd('set packpath^=~/.local/share/nvim-nightly/site')
-vim.cmd('set packpath+=~/.local/share/nvim-nightly/site/after')
-
 -- Runtime Path
 vim.cmd('set runtimepath-=~/.config/nvim')
 vim.cmd('set runtimepath-=~/.config/nvim/after')
@@ -26,10 +10,28 @@ vim.cmd('set runtimepath-=/usr/share/nvim/site')
 vim.cmd('set runtimepath-=/usr/share/nvim/site/after')
 vim.cmd('set runtimepath-=/usr/local/share/nvim/site')
 vim.cmd('set runtimepath-=/usr/local/share/nvim/site/after')
+
 vim.cmd('set runtimepath+=~/.config/nvim-nightly/after')
 vim.cmd('set runtimepath^=~/.config/nvim-nightly')
 vim.cmd('set runtimepath+=~/.local/share/nvim-nightly/site/after')
 vim.cmd('set runtimepath^=~/.local/share/nvim-nightly/site')
+
+-- Pack Path
+vim.cmd('set packpath-=~/.config/nvim')
+vim.cmd('set packpath-=~/.config/nvim/after')
+vim.cmd('set packpath-=~/.local/share/nvim/site')
+vim.cmd('set packpath-=~/.local/share/nvim/site/after')
+vim.cmd('set packpath-=/etc/xdg/nvim')
+vim.cmd('set packpath-=/etc/xdg/nvim/after')
+vim.cmd('set packpath-=/usr/local/share/nvim/site')
+vim.cmd('set packpath-=/usr/local/share/nvim/site/after')
+vim.cmd('set packpath-=/usr/share/nvim/site')
+vim.cmd('set packpath-=/usr/share/nvim/site/after')
+
+vim.cmd('set packpath^=~/.config/nvim-nightly')
+vim.cmd('set packpath+=~/.config/nvim-nightly/after')
+vim.cmd('set packpath^=~/.local/share/nvim-nightly/site')
+vim.cmd('set packpath+=~/.local/share/nvim-nightly/site/after')
 
 -- =============================================================================
 -- = Plugins =
