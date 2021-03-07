a347f91c0fe3b2dcb538f57f09f72c864e878716
Arnold Chand <creativenull@outlook.com>
2021-03-06 23:05:03 -0400

    feat: use separate find files for nvim config dir

--- .config/nvim-nightly/lua/creativenull/plugins/config/telescope.lua
+++ .config/nvim-nightly/lua/creativenull/plugins/config/telescope.lua
@@ -3,28 +3,37 @@ local telescope_builtin = require 'telescope.builtin'
 local telescope_actions = require 'telescope.actions'
 local M = {}
 
-telescope.setup {
-  defaults = {
-    layout_strategy = 'vertical',
-    use_less = false,
-    mappings = {
-      i = {
-        ['<C-k>'] = telescope_actions.move_selection_previous,
-        ['<C-j>'] = telescope_actions.move_selection_next
+M.config = function()
+  telescope.setup {
+    defaults = {
+      layout_strategy = 'vertical',
+      use_less = false,
+      mappings = {
+        i = {
+          ['<C-k>'] = telescope_actions.move_selection_previous,
+          ['<C-j>'] = telescope_actions.move_selection_next
+        }
       }
     }
   }
-}
+end
 
-function M.find_files()
-  local config_dir = os.getenv('HOME') .. '/.config/nvim-nightly'
+M.find_project_files = function()
   telescope_builtin.find_files {
-    find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden', '.', config_dir },
+    find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' },
     previewer = false
   }
 end
 
-function M.live_grep()
+M.find_vim_config_files = function()
+  local config_dir = os.getenv('HOME') .. '/.config/nvim-nightly'
+  telescope_builtin.find_files {
+    find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden', config_dir },
+    previewer = false
+  }
+end
+
+M.live_grep = function()
   telescope_builtin.live_grep {}
 end
 
