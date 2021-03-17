434621e6fec38841cd2d53807e03685347b3b058
Arnold Chand <creativenull@outlook.com>
2021-03-17 10:06:24 -0400

    feat: use nvim_exec() for multiline execution

--- .config/nvim-nightly/lua/creativenull/autocmds.lua
+++ .config/nvim-nightly/lua/creativenull/autocmds.lua
@@ -1,29 +1,37 @@
 -- Yank highlight
-vim.cmd 'augroup yank_hl'
-vim.cmd 'au!'
-vim.cmd 'au TextYankPost * silent! lua vim.highlight.on_yank({ higroup = "Search", timeout = 500 })'
-vim.cmd 'augroup end'
+vim.api.nvim_exec([[
+    augroup yank_hl
+    au!
+    au TextYankPost * silent! lua vim.highlight.on_yank({ higroup = "Search", timeout = 500 })
+    augroup end
+  ]], false)
 
 -- Statusline/Tabline highlights
-vim.cmd 'augroup statusline_tabline_hl'
-vim.cmd 'au!'
-vim.cmd 'au ColorScheme * lua require"creativenull.statusline".set_highlights()'
-vim.cmd 'au ColorScheme * lua require"creativenull.tabline".set_highlights()'
-vim.cmd 'augroup end'
+vim.api.nvim_exec([[
+    augroup statusline_tabline_hl
+    au!
+    au ColorScheme * lua require 'creativenull.statusline'.set_highlights()
+    au ColorScheme * lua require 'creativenull.tabline'.set_highlights()
+    augroup end
+  ]], false)
 
 -- Statusline
-vim.cmd 'augroup statusline_local'
-vim.cmd 'au!'
-vim.cmd 'au WinEnter * lua require"creativenull.statusline".setlocal_active_statusline()'
-vim.cmd 'au WinLeave * lua require"creativenull.statusline".setlocal_inactive_statusline()'
-vim.cmd 'augroup end'
+vim.api.nvim_exec([[
+    augroup statusline_local
+    au!
+    au WinEnter * lua require 'creativenull.statusline'.setlocal_active_statusline()
+    au WinLeave * lua require 'creativenull.statusline'.setlocal_inactive_statusline()
+    augroup end
+  ]], false)
 
  -- Enable invisible chars
-vim.cmd 'augroup disable_char_list'
-vim.cmd 'au!'
-vim.cmd 'au FileType help setlocal nolist'
-vim.cmd 'au FileType TelescopePrompt setlocal nolist'
-vim.cmd 'augroup end'
+vim.api.nvim_exec([[
+    augroup disable_char_list
+    au!
+    au FileType help setlocal nolist
+    au FileType TelescopePrompt setlocal nolist
+    augroup end
+  ]], false)
 
 -- lua 2 space indent
 vim.cmd 'au! FileType lua setlocal tabstop=2'
