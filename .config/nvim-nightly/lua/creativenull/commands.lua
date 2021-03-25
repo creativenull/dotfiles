bc52d4903f56f5d2c18805f3747252d3b0aa7933
Arnold Chand <creativenull@outlook.com>
2021-03-25 14:45:03 -0400

    fix: formatting

--- .config/nvim-nightly/lua/creativenull/commands.lua
+++ .config/nvim-nightly/lua/creativenull/commands.lua
@@ -1,4 +1,7 @@
-vim.cmd 'command! Config edit $MYVIMRC'
+-- Toggle MD/Json quotes
+vim.cmd [[command! ToggleConceal lua require'creativenull.utils'.toggle_conceal()]]
+
+-- Reload config
 vim.cmd [[command! ConfigReload lua require'creativenull.utils'.reload_config()]]
 
 -- I can't release my shift key fast enough :')
@@ -7,6 +10,3 @@ vim.cmd 'command! -nargs=* Wq wq'
 vim.cmd 'command! -nargs=* Q q'
 vim.cmd 'command! -nargs=* Qa qa'
 vim.cmd 'command! -nargs=* QA qa'
-
--- Toggle MD/Json quotes
-vim.cmd [[command! ToggleConceal lua require'creativenull.utils'.toggle_conceal()]]
--- .config/nvim-nightly/lua/creativenull/options.lua
+++ .config/nvim-nightly/lua/creativenull/options.lua
@@ -1,81 +1,75 @@
-local o = vim.o -- global
-local wo = vim.wo -- window
-
 -- Completion options
-if string.match(o.shortmess, 'c') == nil then
-  -- Append only once on source
-  o.shortmess = vim.o.shortmess .. 'c'
-end
-o.completeopt = 'menuone,noinsert,noselect'
-o.updatetime = 100
+if string.match(vim.o.shortmess, 'c') == nil then vim.o.shortmess = vim.o.shortmess .. 'c' end
+vim.o.completeopt = 'menuone,noinsert,noselect'
+vim.o.updatetime = 100
 
 -- Search options
-o.hlsearch = true
-o.incsearch = true
-o.ignorecase = true
-o.smartcase = true
-o.wrapscan = true
+vim.o.hlsearch = true
+vim.o.incsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.wrapscan = true
 
 -- Indent options
-o.tabstop = 4
-o.shiftwidth = 0
-o.softtabstop = 4
-o.expandtab = true
-o.autoindent = true
-o.smartindent = true
-o.smarttab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 0
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.smarttab = true
 
 -- Line options
-o.showmatch = true
-o.showbreak = '+++'
-o.textwidth = 120
-o.scrolloff = 5
-wo.linebreak = true
-wo.colorcolumn = '120' -- some prefer 80, but I just like to break the rules :)
+vim.o.showmatch = true
+vim.o.showbreak = '+++'
+vim.o.textwidth = 120
+vim.o.scrolloff = 5
+vim.wo.linebreak = true
+vim.wo.colorcolumn = '120' -- some prefer 80, but I just like to break the rules :)
 
 -- Move swapfiles and backups into cache
-o.directory = os.getenv('HOME') .. '/.cache/nvim-nightly'
-o.backup = true
-o.backupdir = os.getenv('HOME') .. '/.cache/nvim-nightly'
+vim.o.directory = os.getenv('HOME') .. '/.cache/nvim-nightly'
+vim.o.backup = true
+vim.o.backupdir = os.getenv('HOME') .. '/.cache/nvim-nightly'
 
 -- Enable the integrated undo features
-o.undofile = true
-o.undodir = os.getenv('HOME') .. '/.cache/nvim-nightly'
-o.undolevels = 1000
-o.history = 1000
+vim.o.undofile = true
+vim.o.undodir = os.getenv('HOME') .. '/.cache/nvim-nightly'
+vim.o.undolevels = 1000
+vim.o.history = 1000
 
 -- Lazy redraw
-o.lazyredraw = true
+vim.o.lazyredraw = true
 
 -- Buffers/Tabs/Windows
-o.hidden = true
+vim.o.hidden = true
 
 -- Set spelling
-o.spell = false
+vim.o.spell = false
 
 -- For git
-wo.signcolumn = 'yes'
+vim.wo.signcolumn = 'yes'
 
 -- Mouse support
-o.mouse = 'a'
+vim.o.mouse = 'a'
 
 -- backspace behaviour
-o.backspace = 'indent,eol,start'
+vim.o.backspace = 'indent,eol,start'
 
 -- Status line
-o.showmode = false
-o.statusline = require 'creativenull.statusline'.get_statusline()
+vim.o.showmode = false
+vim.o.statusline = require 'creativenull.statusline'.get_statusline()
 
 -- Tab line
-o.showtabline = 2
-o.tabline = require 'creativenull.tabline'.get_tabline()
+vim.o.showtabline = 2
+vim.o.tabline = require 'creativenull.tabline'.get_tabline()
 
 -- Better display
-o.cmdheight = 2
+vim.o.cmdheight = 2
 
 -- Auto reload file if changed outside vim, or just :e!
-o.autoread = true
+vim.o.autoread = true
 
 -- Invisible chars list
-wo.list = true
-o.listchars = [[tab:▸ ,trail:·,space:·]]
+vim.wo.list = true
+vim.o.listchars = [[tab:▸ ,trail:·,space:·]]
