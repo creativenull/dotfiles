20a86d1ca36b0ca1034b9ab3b2806498de5fcf3f
Arnold Chand <creativenull@outlook.com>
2021-03-29 18:38:37 -0400

    fix(doom): update font size

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -20,8 +20,8 @@
 ;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 18 :weight 'regular)
-      doom-variable-pitch-font (font-spec :family "sans" :size 18))
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 14 :weight 'regular)
+      doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
