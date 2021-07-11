910deae17d0d22c3edc81729d3b6a29d43fb479b
Arnold Chand <creativenull@outlook.com>
2021-07-11 13:52:40 -0400

    feat(nvim-nightly): organize options by capability

--- .config/nvim-nightly/init.lua
+++ .config/nvim-nightly/init.lua
@@ -112,54 +112,48 @@ options_init()
 -- Completion options
 vim.opt.completeopt = {'menuone', 'noinsert', 'noselect'}
 vim.opt.shortmess:append('c')
-vim.opt.updatetime = 250
 
--- Search options
+-- Search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
 
--- Indent options
+-- Editor
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.tabstop = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.smarttab = true
-
--- Line options
+vim.opt.autoindent = true
 vim.opt.showmatch = true
 vim.opt.wrap = false
 vim.opt.colorcolumn = '120'
 vim.opt.scrolloff = 5
+vim.opt.lazyredraw = true
+vim.opt.spell = false
 
--- No backups and swapfiles but undo tree
+-- System
+vim.opt.encoding = 'utf-8'
 vim.opt.backup = false
 vim.opt.swapfile = false
+vim.opt.updatetime = 250
 vim.opt.undofile = true
 vim.opt.undodir = undodir
 vim.opt.undolevels = 10000
 vim.opt.history = 10000
-
--- Performance gainzzz
-vim.opt.lazyredraw = true
-
--- Hide a buffer when abandoned using :bd
-vim.opt.hidden = true
-
--- Spelling only for markdown
-vim.opt.spell = false
-
--- For git signs
-vim.opt.signcolumn = 'yes'
-
--- Show 2 lines of command output on the command line below
-vim.opt.cmdheight = 2
-
--- Always show the tabline on top
-vim.opt.showtabline = 2
-
--- Use system/OS clipboard by default
+vim.opt.backspace = 'indent,eol,start'
 vim.opt.clipboard = 'unnamedplus'
+vim.opt.ttimeoutlen = 50
+vim.opt.mouse = ''
+
+-- UI
+vim.opt.hidden = true
+vim.opt.signcolumn = 'yes'
+vim.opt.cmdheight = 2
+vim.opt.showtabline = 2
+vim.opt.laststatus = 2
 
 -- =============================================================================
 -- = Commands =
