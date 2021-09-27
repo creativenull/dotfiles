d90e5f00a0f00e823e839f100b7537ae90271952
Arnold Chand <creativenull@outlook.com>
2021-09-27 14:18:35 -0400

    feat(nvim-nightly): more autocompletion updates

--- .config/nvim-nightly/lua/cnull/autocompletion.lua
+++ .config/nvim-nightly/lua/cnull/autocompletion.lua
@@ -7,9 +7,7 @@ ddc_patch_global({
   sources = {'nvim-lsp', 'ultisnips', 'around', 'buffer'},
   sourceOptions = {
     ['_'] = {
-      -- matchers = {'matcher_fuzzy', 'matcher_length'},
       matchers = {'matcher_fuzzy'},
-      -- sorters = {'sorter_rank'},
       sorters = {'sorter_fuzzy'},
       ignoreCase = true,
     },
@@ -28,23 +26,3 @@ ddc_patch_global({
     },
   },
 })
-
---[[ ddc_patch_global('autoCompleteDelay', 100)
-ddc_patch_global('sources', {'nvim-lsp', 'ultisnips', 'around'})
-ddc_patch_global('sourceOptions', {
-  ['_'] = {
-    matchers = {'matcher_fuzzy', 'matcher_length'},
-    sorters = {'sorter_rank'},
-    ignoreCase = true,
-  },
-  ['ultisnips'] = {
-    mark = 'ultisnips',
-  },
-  ['nvim-lsp'] = {
-    mark = 'lsp',
-    forceCompletionPattern = '\\.|:|->',
-  },
-  ['around'] = {
-    mark = 'around',
-  },
-}) ]]
