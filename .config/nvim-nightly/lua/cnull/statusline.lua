3352550d9be38af9dc6e327a7b52bfe00d8e91fe
Arnold Chand <creativenull@outlook.com>
2021-10-23 16:15:15 -0400

    feat(nvim-nightly): new lualine settings

--- .config/nvim-nightly/lua/cnull/statusline.lua
+++ .config/nvim-nightly/lua/cnull/statusline.lua
@@ -1,12 +1,12 @@
 local lualine = require('lualine')
-local custom_theme = require('lualine.themes.powerline')
-custom_theme.normal.a = {}
-custom_theme.normal.a.bg = '#047857'
-custom_theme.normal.z = {}
-custom_theme.normal.z.bg = '#444444'
-custom_theme.insert.z = {}
-custom_theme.insert.z.bg = custom_theme.insert.a.bg
-custom_theme.insert.z.fg = custom_theme.insert.a.fg
+local custom_powerline = require('lualine.themes.powerline')
+custom_powerline.normal.a = {}
+custom_powerline.normal.a.bg = '#047857'
+custom_powerline.normal.z = {}
+custom_powerline.normal.z.bg = '#444444'
+custom_powerline.insert.z = {}
+custom_powerline.insert.z.bg = custom_powerline.insert.a.bg
+custom_powerline.insert.z.fg = custom_powerline.insert.a.fg
 
 -- Line Info Component
 local function line_info_component()
@@ -30,9 +30,9 @@ end
 lualine.setup({
   options = {
     icons_enabled = true,
-    theme = 'powerline',
-    section_separators = { '', '' },
-    disabled_filetypes = { 'TelescopePrompt', 'nnn' },
+    theme = custom_powerline,
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = { 'TelescopePrompt' },
   },
 
   sections = {
@@ -48,6 +48,12 @@ lualine.setup({
         'diagnostics',
         sources = { 'nvim_lsp' },
         sections = { 'error', 'warn' },
+        diagnostics_color = {
+          error = { fg = '#ff6666' },
+          warn = { fg = '#eeee99' },
+          info = nil,
+          hint = nil,
+        },
       },
     },
   },
