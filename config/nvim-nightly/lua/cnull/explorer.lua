e34fb5b1624d3d3c8311b1f61fe29ab37379fbc7
Arnold Chand <creativenull@outlook.com>
2021-12-01 09:35:35 -0400

    feat(nvim-nightly): file explorer addition

---
+++ config/nvim-nightly/lua/cnull/explorer.lua
@@ -0,0 +1,52 @@
+local lir = require('lir')
+local lir_actions = require('lir.actions')
+local lir_mark_actions = require('lir.mark.actions')
+local lir_clipboard_actions = require('lir.clipboard.actions')
+
+lir.setup({
+  show_hidden_files = true,
+  devicons_enable = true,
+  hide_cursor = true,
+  float = {
+    winblend = 0,
+    win_opts = function()
+      return {
+        border = 'rounded',
+      }
+    end,
+  },
+  mappings = {
+    ['l'] = lir_actions.edit,
+    ['<CR>'] = lir_actions.edit,
+    ['<C-s>'] = lir_actions.split,
+    ['<C-v>'] = lir_actions.vsplit,
+    ['<C-t>'] = lir_actions.tabedit,
+
+    ['h'] = lir_actions.up,
+    ['q'] = lir_actions.quit,
+
+    ['K'] = lir_actions.mkdir,
+    ['N'] = lir_actions.newfile,
+    ['R'] = lir_actions.rename,
+    ['@'] = lir_actions.cd,
+    ['Y'] = lir_actions.yank_path,
+    ['.'] = lir_actions.toggle_show_hidden,
+    ['D'] = lir_actions.delete,
+
+    ['J'] = function()
+      lir_mark_actions.toggle_mark()
+      vim.cmd('normal! j')
+    end,
+
+    ['C'] = lir_clipboard_actions.copy,
+    ['X'] = lir_clipboard_actions.cut,
+    ['P'] = lir_clipboard_actions.paste,
+  },
+})
+
+vim.api.nvim_set_keymap(
+  'n',
+  '<Leader>ff',
+  [[<Cmd>lua require('lir.float').toggle()<CR>]],
+  { noremap = true, silent = true }
+)
