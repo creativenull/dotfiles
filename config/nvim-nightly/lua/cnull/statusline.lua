ca350d95769f8de174a3e227d9e0e6ff01d0c799
Arnold Chand <creativenull@outlook.com>
2022-02-14 17:21:06 -0400

    feat(nvim): use same theme as stable

--- config/nvim-nightly/lua/cnull/statusline.lua
+++ config/nvim-nightly/lua/cnull/statusline.lua
@@ -1,12 +1,85 @@
-local lualine = require('lualine')
-local custom_powerline = require('lualine.themes.powerline')
-custom_powerline.normal.a = {}
-custom_powerline.normal.a.bg = '#047857'
-custom_powerline.normal.z = {}
-custom_powerline.normal.z.bg = '#444444'
-custom_powerline.insert.z = {}
-custom_powerline.insert.z.bg = custom_powerline.insert.a.bg
-custom_powerline.insert.z.fg = custom_powerline.insert.a.fg
+local base_colors = {
+  light = '#f1f5f9',
+  dark = '#111827',
+  softlight = '#cbd5e1',
+  softdark = '#334155',
+}
+
+local colors = {
+  text_light = base_colors.light,
+  text_dark = base_colors.dark,
+  text_softlight = base_colors.softlight,
+  text_softdark = base_colors.softdark,
+
+  bg_light = base_colors.light,
+  bg_dark = base_colors.dark,
+  bg_softlight = base_colors.softlight,
+  bg_softdark = base_colors.softdark,
+
+  primary = '#0369a1',
+  indigo = '#4f46e5',
+  gray = '#1f2937',
+  darkgray = '#111827',
+
+  insert_primary = '#0c4a6e',
+  visual_primary = '#92400e',
+  command_primary = '#94a3b8',
+  replace_primary = '#94a3b8',
+
+  error = '#881337',
+  warn = '#fdba74',
+}
+
+local theme = {
+  normal = {
+    a = { fg = colors.text_softlight, bg = colors.primary },
+    b = { fg = colors.text_softlight, bg = colors.darkgray },
+    c = { fg = 'NONE', bg = colors.bg_dark },
+    x = { fg = colors.text_light, bg = colors.darkgray },
+    y = { fg = colors.text_light, bg = colors.gray },
+    z = { fg = colors.text_light, bg = colors.indigo },
+  },
+  insert = {
+    a = { fg = colors.text_softlight, bg = colors.insert_primary },
+    b = { fg = colors.text_softlight, bg = colors.darkgray },
+    c = { fg = 'NONE', bg = colors.insert_primary },
+    x = { fg = colors.text_light, bg = colors.insert_primary },
+    y = { fg = colors.text_light, bg = colors.gray },
+    z = { fg = colors.text_light, bg = colors.indigo },
+  },
+  visual = {
+    a = { fg = colors.text_softlight, bg = colors.visual_primary },
+    b = { fg = colors.text_softlight, bg = colors.darkgray },
+    c = { fg = 'NONE', bg = colors.visual_primary },
+    x = { fg = colors.text_light, bg = colors.visual_primary },
+    y = { fg = colors.text_light, bg = colors.gray },
+    z = { fg = colors.text_light, bg = colors.indigo },
+  },
+  command = {
+    a = { fg = colors.text_softdark, bg = colors.command_primary },
+    b = { fg = colors.text_softlight, bg = colors.darkgray },
+    c = { fg = 'NONE', bg = colors.command_primary },
+    x = { fg = colors.text_dark, bg = colors.command_primary },
+    y = { fg = colors.text_light, bg = colors.gray },
+    z = { fg = colors.text_light, bg = colors.indigo },
+  },
+  replace = {
+    a = { fg = colors.text_softdark, bg = colors.replace_primary },
+    b = { fg = colors.text_softlight, bg = colors.darkgray },
+    c = { fg = 'NONE', bg = colors.replace_primary },
+    x = { fg = colors.text_dark, bg = colors.replace_primary },
+    y = { fg = colors.text_light, bg = colors.gray },
+    z = { fg = colors.text_light, bg = colors.indigo },
+  },
+  inactive = {
+    a = { fg = colors.text_softlight, bg = colors.bg_softdark },
+    b = { fg = colors.text_softlight, bg = colors.bg_softdark },
+    c = { fg = 'NONE', bg = colors.bg_dark },
+    x = { fg = colors.text_softlight, bg = colors.bg_softdark },
+    y = { fg = colors.text_softlight, bg = colors.bg_softdark },
+    z = { fg = colors.text_softlight, bg = colors.bg_softdark },
+  },
+}
 
 -- Line Info Component
 local function line_info_component()
@@ -27,10 +100,38 @@ local function lsp_ready_component()
   end
 end
 
-lualine.setup({
+local function err_diagnostic_component()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local severity = vim.diagnostic.severity.ERROR
+  local diagnostics = vim.diagnostic.get(bufnr, { severity = severity })
+  return vim.tbl_count(diagnostics)
+end
+
+local function is_err_diagnostic()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local severity = vim.diagnostic.severity.ERROR
+  local diagnostics = vim.diagnostic.get(bufnr, { severity = severity })
+  return vim.tbl_count(diagnostics) > 0
+end
+
+local function warn_diagnostic_component()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local severity = vim.diagnostic.severity.WARN
+  local diagnostics = vim.diagnostic.get(bufnr, { severity = severity })
+  return vim.tbl_count(diagnostics)
+end
+
+local function is_warn_diagnostic()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local severity = vim.diagnostic.severity.WARN
+  local diagnostics = vim.diagnostic.get(bufnr, { severity = severity })
+  return vim.tbl_count(diagnostics) > 0
+end
+
+require('lualine').setup({
   options = {
     icons_enabled = true,
-    theme = custom_powerline,
+    theme = theme,
     section_separators = { left = '', right = '' },
     disabled_filetypes = { 'TelescopePrompt' },
   },
@@ -45,15 +146,16 @@ lualine.setup({
     lualine_z = {
       lsp_ready_component,
       {
-        'diagnostics',
-        sources = { 'nvim_diagnostic' },
-        sections = { 'error', 'warn' },
-        diagnostics_color = {
-          error = { fg = '#ff6666' },
-          warn = { fg = '#eeee99' },
-          info = nil,
-          hint = nil,
-        },
+        err_diagnostic_component,
+        cond = is_err_diagnostic,
+        separator = { left = '' },
+        color = { fg = colors.text_softlight, bg = colors.error }
+      },
+      {
+        warn_diagnostic_component,
+        cond = is_warn_diagnostic,
+        separator = { left = '' },
+        color = { fg = colors.text_softdark, bg = colors.warn }
       },
     },
   },
