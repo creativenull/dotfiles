07d8e50ecc95577828688d9a950474f4dc8b67d2
Arnold Chand <creativenull@outlook.com>
2022-05-09 12:55:06 -0400

    feat(nvim): 0.7 features

--- config/nvim/lua/cnull/lsp.lua
+++ config/nvim/lua/cnull/lsp.lua
@@ -1,17 +1,5 @@
-local augroup = require('cnull.shared.event').augroup
-local keymap = require('cnull.shared.keymap').keymap
-local buf_keymap = require('cnull.shared.keymap').buf_keymap
-local border = 'rounded'
-local width = 80
-
-augroup.set('lsp_user_events', function(autocmd)
-  autocmd.set('CursorHold', '*', function()
-    vim.diagnostic.open_float(nil, {
-      width = width,
-      border = border,
-    })
-  end, { desc = 'Show LSP Line Diagnostic' })
-end)
+local BORDER = 'rounded'
+local WIDTH = 80
 
 ---Statusline component to check if the LSP server connected to the buffer
 ---@return string
@@ -33,28 +21,32 @@ local function on_attach(client, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- LSP Keymaps
-  buf_keymap.set(bufnr, 'n', '<Leader>la', vim.lsp.buf.code_action, { desc = 'LSP Code Actions' })
-  buf_keymap.set(bufnr, 'n', '<Leader>ld', vim.lsp.buf.definition, { desc = 'LSP Go-to Definition' })
-  buf_keymap.set(bufnr, 'n', '<Leader>lh', vim.lsp.buf.hover, { desc = 'LSP Hover Information' })
-  buf_keymap.set(bufnr, 'n', '<Leader>lr', vim.lsp.buf.rename, { desc = 'LSP Rename' })
-  buf_keymap.set(bufnr, 'n', '<Leader>ls', vim.lsp.buf.signature_help, { desc = 'LSP Signature Help' })
-  buf_keymap.set(bufnr, 'n', '<Leader>le', vim.diagnostic.setloclist, { desc = 'LSP Show All Diagnostics' })
-  buf_keymap.set(bufnr, 'n', '<Leader>lw', function()
+  vim.keymap.set('n', '<Leader>la', vim.lsp.buf.code_action, { desc = 'LSP Code Actions', buffer = bufnr })
+  vim.keymap.set('n', '<Leader>ld', vim.lsp.buf.definition, { desc = 'LSP Go-to Definition', buffer  = bufnr })
+  vim.keymap.set('n', '<Leader>lh', vim.lsp.buf.hover, { desc = 'LSP Hover Information', buffer  = bufnr })
+  vim.keymap.set('n', '<Leader>lr', vim.lsp.buf.rename, { desc = 'LSP Rename', buffer  = bufnr })
+  vim.keymap.set('n', '<Leader>ls', vim.lsp.buf.signature_help, { desc = 'LSP Signature Help', buffer  = bufnr })
+  vim.keymap.set('n', '<Leader>le', vim.diagnostic.setloclist, { desc = 'LSP Show All Diagnostics', buffer  = bufnr })
+  vim.keymap.set('n', '<Leader>lw', function()
     vim.diagnostic.open_float({
       bufnr = bufnr,
-      width = width,
-      border = border,
+      width = WIDTH,
+      border = BORDER,
     })
-  end, { desc = 'Show LSP Line Diagnostic' })
+  end, { desc = 'Show LSP Line Diagnostic', buffer  = bufnr })
 
-  -- LSP Formatting keymap only from the following
-  -- linter/formatter servers
-  local utility_servers = { 'diagnosticls', 'efm', 'null-ls' }
+  -- LSP Formatting keymap only from the following linter/formatter servers
+  -- Ref: https://github.com/neovim/nvim-lspconfig/wiki/Multiple-language-servers-FAQ
+  local allowed_fmt_servers = { 'diagnosticls', 'efm', 'null-ls' }
 
-  if vim.tbl_contains(utility_servers, client.name) then
-    buf_keymap.set(bufnr, 'n', '<Leader>lf', function()
+  if vim.tbl_contains(allowed_fmt_servers, client.name) then
+    local desc = string.format('LSP Formatting with %s', client.name)
+
+    local function client_fmt()
       client.request('textDocument/formatting', vim.lsp.util.make_formatting_params({}), nil, bufnr)
-    end, { desc = 'LSP Formatting' })
+    end
+
+    vim.keymap.set('n', '<Leader>lf', client_fmt, { desc = desc, buffer = bufnr })
   end
 end
 
@@ -85,19 +77,19 @@ vim.diagnostic.config({
 vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
   vim.lsp.handlers.hover,
   {
-    width = width,
-    border = border,
+    width = WIDTH,
+    border = BORDER,
   }
 )
 
 -- Add border to signature help
 vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
   vim.lsp.handlers.signature_help,
-  { border = border }
+  { border = BORDER }
 )
 
 -- Check registered LSP info
-keymap.set('n', '<Leader>li', '<Cmd>LspInfo<CR>')
+vim.keymap.set('n', '<Leader>li', '<Cmd>LspInfo<CR>')
 
 -- Log debug
 -- vim.lsp.set_log_level('debug')
--- config/nvim/lua/cnull/shared/handlers/err.lua
+++
@@ -1,6 +0,0 @@
----Print error to nvim
----@param msg string
----@return nil
-return function(msg)
-  vim.api.nvim_err_writeln(msg)
-end
