cdcf3eb2f68f0e27f5f9b88b7571eb8c655183ba
Arnold Chand <creativenull@outlook.com>
2022-08-25 14:11:30 -0300

    fix(nvim): check if qargs is provided

--- config/nvim/lua/user/fzf.lua
+++ config/nvim/lua/user/fzf.lua
@@ -1,11 +1,16 @@
 local M = {}
 
-local function vimGrep(qargs, bang)
-  local sh = 'rg --column --line-number --no-heading --color=always --smart-case -- ' .. vim.fn.shellescape(qargs)
+local function vim_grep(qargs, bang)
+	local query = '""'
+	if qargs ~= nil then
+		query = vim.fn.shellescape(qargs)
+	end
+
+  local sh = 'rg --column --line-number --no-heading --color=always --smart-case -- ' .. query
   vim.call('fzf#vim#grep', sh, 1, vim.call('fzf#vim#with_preview', 'right:50%', 'ctrl-/'), bang)
 end
 
-local function fzfWindowSetup()
+local function fzf_window_setup()
   local buf = vim.api.nvim_get_current_buf()
   vim.opt.ruler = false
 
@@ -25,7 +30,7 @@ function M.Setup()
 
   -- Commands
   vim.api.nvim_create_user_command('Rg', function(arg)
-    vimGrep(arg.qargs, arg.bang)
+    vim_grep(arg.qargs, arg.bang)
   end, { bang = true, nargs = '*' })
 
   -- Keymaps
@@ -36,7 +41,7 @@ function M.Setup()
   vim.api.nvim_create_autocmd('FileType', {
     group = vim.g.user.event,
     pattern = 'fzf',
-    callback = fzfWindowSetup,
+    callback = fzf_window_setup,
     desc = 'Configure fzf window before it is viewed',
   })
 
