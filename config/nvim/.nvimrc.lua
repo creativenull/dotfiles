3c214e02e13163ea8b2863550769fbdf40cee3ac
Arnold Chand <creativenull@outlook.com>
2022-09-01 13:30:54 -0400

    feat(nvim): pcall to ensure no errors happen

--- config/nvim/.nvimrc.lua
+++ config/nvim/.nvimrc.lua
@@ -1,36 +1,39 @@
 -- Powered by projectlocal-vim
 -- https://github.com/creativenull/projectlocal-vim
-local projectlocal = require('projectlocal.lsp')
-local lspconfig = require('lspconfig')
-local root_pattern = require('lspconfig').util.root_pattern
+local function setup()
+  local projectlocal = require('projectlocal.lsp')
+  local lspconfig = require('lspconfig')
+  local root_pattern = require('lspconfig').util.root_pattern
 
--- ALE Config
-vim.api.nvim_create_augroup('ALEUserEvents', { clear = true })
-vim.api.nvim_create_autocmd('FileType', {
-  group = 'ALEUserEvents',
-  pattern = 'lua',
-  callback = function()
-    local buf = vim.api.nvim_get_current_buf()
-    vim.b[buf].ale_linters = { 'luacheck' }
-    vim.b[buf].ale_fixers = { 'stylua' }
-  end
-})
+  -- ALE Config
+  vim.api.nvim_create_augroup('ALEUserEvents', { clear = true })
+  vim.api.nvim_create_autocmd('FileType', {
+    group = 'ALEUserEvents',
+    pattern = 'lua',
+    callback = function(args)
+      vim.b[args.buf].ale_linters = { 'luacheck' }
+      vim.b[args.buf].ale_fixers = { 'stylua' }
+    end,
+  })
 
--- Lua LSP
-local lua_rtp = vim.split(package.path, ';')
-table.insert(lua_rtp, 'lua/?.lua')
-table.insert(lua_rtp, 'lua/?/init.lua')
-lspconfig.sumneko_lua.setup(projectlocal.get_config({
-  root_dir = root_pattern('.git'),
-  settings = {
-    Lua = {
-      runtime = {
-        version = 'LuaJIT',
-        path = lua_rtp,
+  -- Lua LSP
+  local lua_rtp = vim.split(package.path, ';')
+  table.insert(lua_rtp, 'lua/?.lua')
+  table.insert(lua_rtp, 'lua/?/init.lua')
+  lspconfig.sumneko_lua.setup(projectlocal.get_config({
+    root_dir = root_pattern('.git'),
+    settings = {
+      Lua = {
+        runtime = {
+          version = 'LuaJIT',
+          path = lua_rtp,
+        },
+        diagnostics = { globals = { 'vim' } },
+        workspace = { library = vim.api.nvim_get_runtime_file('', true) },
+        telemetry = { enable = false },
       },
-      diagnostics = { globals = { 'vim' } },
-      workspace = { library = vim.api.nvim_get_runtime_file('', true) },
-      telemetry = { enable = false },
     },
-  },
-}))
+  }))
+end
+
+pcall(setup)
