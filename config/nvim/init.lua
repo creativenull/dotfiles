9b8d486f953d624d9f0223d9df65dd3016ccda98
Arnold Chand <creativenull@outlook.com>
2022-09-05 14:04:53 -0400

    feat(nvim): save last cursor position

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -81,6 +81,24 @@ vim.g.loaded_perl_provider = 0
 -- Global user group to register other custom autocmds
 vim.api.nvim_create_augroup(vim.g.user.event, {})
 
+-- From vim defaults.vim
+-- ---
+-- When editing a file, always jump to the last known cursor position.
+-- Don't do it when the position is invalid, when inside an event handler
+-- (happens when dropping a file on gvim) and for a commit message (it's
+-- likely a different one than last time).
+vim.api.nvim_create_autocmd('BufReadPost', {
+	group = vim.g.user.event,
+	callback = function(args)
+		local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line('$')
+		local not_commit = vim.b[args.buf].filetype ~= 'commit'
+
+		if valid_line and not_commit then
+			vim.cmd([[normal! g`"]])
+		end
+	end,
+})
+
 vim.api.nvim_create_autocmd('FileType', {
   group = vim.g.user.event,
   pattern = {
