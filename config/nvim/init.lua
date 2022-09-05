c5e4365511719279ee280287169cd090860b85ad
Arnold Chand <creativenull@outlook.com>
2022-09-05 14:08:50 -0400

    fix(nvim): formatting

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -88,15 +88,15 @@ vim.api.nvim_create_augroup(vim.g.user.event, {})
 -- (happens when dropping a file on gvim) and for a commit message (it's
 -- likely a different one than last time).
 vim.api.nvim_create_autocmd('BufReadPost', {
-	group = vim.g.user.event,
-	callback = function(args)
-		local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line('$')
-		local not_commit = vim.b[args.buf].filetype ~= 'commit'
+  group = vim.g.user.event,
+  callback = function(args)
+    local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line('$')
+    local not_commit = vim.b[args.buf].filetype ~= 'commit'
 
-		if valid_line and not_commit then
-			vim.cmd([[normal! g`"]])
-		end
-	end,
+    if valid_line and not_commit then
+      vim.cmd([[normal! g`"]])
+    end
+  end,
 })
 
 vim.api.nvim_create_autocmd('FileType', {
