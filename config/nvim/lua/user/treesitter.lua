830b151753aef6bfafa836f8349ff1c7698eacdb
Arnold Chand <creativenull@outlook.com>
2022-09-27 12:40:02 -0400

    feat(nvim): inc selection disable

--- config/nvim/lua/user/treesitter.lua
+++ config/nvim/lua/user/treesitter.lua
@@ -4,7 +4,8 @@ require('nvim-treesitter.configs').setup({
     enable = true,
     additional_vim_regex_highlighting = false,
   },
-  indent = { enable = true },
+  indent = { enable = false },
+  incremental_selection = { enable = false },
 })
 
 vim.opt.foldmethod = 'expr'
