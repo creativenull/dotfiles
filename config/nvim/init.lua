23750cf27f04ec51a306a01a2fa3683406ce11dc
Arnold Chand <creativenull@outlook.com>
2022-10-20 13:24:43 -0400

    fix(nvim): use default fillchars

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -209,15 +209,6 @@ vim.opt.showtabline = 2
 vim.opt.signcolumn = 'yes'
 vim.opt.termguicolors = true
 vim.opt.laststatus = 3
-vim.opt.fillchars:append({
-  horiz = '━',
-  horizup = '┻',
-  horizdown = '┳',
-  vert = '┃',
-  vertleft = '┨',
-  vertright = '┣',
-  verthoriz = '╋',
-})
 
 -- =============================================================================
 -- = Keybindings (KEY) =
