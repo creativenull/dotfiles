ee454bd7ee632e106a9a86bec5de0ccb1d1fb81b
Arnold Chand <creativenull@outlook.com>
2022-12-27 15:47:50 -0400

    feat(nvim): fidget setup

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -569,7 +569,21 @@ require('user.lsp').setup()
 
 -- fidget.nvim Config
 -- ---
-require('fidget').setup({})
+require('fidget').setup({
+  window = {
+    border = 'rounded',
+  },
+  fmt = {
+    -- function to format fidget title
+    fidget = function(fidget_name, spinner)
+      return string.format(' %s %s ', spinner, fidget_name)
+    end,
+    -- function to format each task line
+    task = function(task_name, message, percentage)
+      return string.format(' %s%s [%s] ', message, percentage and string.format(' (%s%%)', percentage) or '', task_name)
+    end,
+  },
+})
 
 -- fzf.vim Config
 -- ---
