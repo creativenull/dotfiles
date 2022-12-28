3a263d0edc8b1838e753d017f14a18f2382aa875
Arnold Chand <creativenull@outlook.com>
2022-12-28 13:00:32 -0400

    fix(nvim): separate fidget setup

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -569,21 +569,7 @@ require('user.lsp').setup()
 
 -- fidget.nvim Config
 -- ---
-require('fidget').setup({
-  window = {
-    border = 'rounded',
-  },
-  fmt = {
-    -- function to format fidget title
-    fidget = function(fidget_name, spinner)
-      return string.format(' %s %s ', spinner, fidget_name)
-    end,
-    -- function to format each task line
-    task = function(task_name, message, percentage)
-      return string.format(' %s%s [%s] ', message, percentage and string.format(' (%s%%)', percentage) or '', task_name)
-    end,
-  },
-})
+require('user.fidget').setup()
 
 -- fzf.vim Config
 -- ---
