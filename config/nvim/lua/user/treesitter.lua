b4493aa2cab393f5fead89b38425f5e6d8d914ad
Arnold Chand <creativenull@outlook.com>
2022-12-30 16:01:16 -0400

    fix(nvim): defaults from repo

--- config/nvim/lua/user/treesitter.lua
+++ config/nvim/lua/user/treesitter.lua
@@ -16,13 +16,18 @@ function M.setup()
       'json',
       'php',
       'typescript',
-      'vim',
       'vue',
       'zig',
     },
     highlight = {
       enable = true,
-      additional_vim_regex_highlighting = false,
+      disable = function(_, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
     },
     indent = { enable = true },
     incremental_selection = { enable = false },
