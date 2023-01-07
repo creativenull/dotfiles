089734516c0d05fdebd875727b739ee91784058f
Arnold Chand <creativenull@outlook.com>
2023-01-06 22:00:24 -0400

    fix(nvim-nightly): cleanup vsnip

--- config/nvim-nightly/init.lua
+++ config/nvim-nightly/init.lua
@@ -634,11 +634,6 @@ require('lazy').setup({
 			-- Cmp
 			local cmp = require 'cmp'
 
-			vim.g.vsnip_filetypes = {
-				javascriptreact = { 'javascript' },
-				typescriptreact = { 'typescript' },
-			}
-
 			local function has_words_before()
 				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 				return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
