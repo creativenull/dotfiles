76a6a076882c9cf4058206c302ca78845edab31c
Arnold Chand <creativenull@outlook.com>
2023-01-09 13:47:26 -0400

    feat(wezterm): initial setup, so far liking it

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require('wezterm')
+
+return {
+  font = wezterm.font('JetBrainsMono Nerd Font'),
+  font_size = 14,
+  line_height = 2,
+  window_padding = {
+    top = 0,
+    right = 0,
+    bottom = 0,
+    left = 0,
+  },
+  colors = {
+    cursor_bg = '#e5e5e5',
+    cursor_fg = '#171717',
+  },
+}
