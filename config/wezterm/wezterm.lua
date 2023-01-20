c32b43b2259cd315face816d35bc2aa39032c3e2
Arnold Chand <creativenull@outlook.com>
2023-01-20 13:36:05 -0400

    feat(wezterm): start maximized

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,6 +1,14 @@
 local wezterm = require('wezterm')
 
+-- Start with a maximized window
+wezterm.on('gui-startup', function()
+  local tab, pane, window = wezterm.mux.spawn_window({})
+  window:gui_window():maximize()
+end)
+
 local font_size = 12
+
+-- Something something retina displays makes text smol
 if string.find(wezterm.target_triple, 'darwin') ~= nil then
   font_size = 14
 end
