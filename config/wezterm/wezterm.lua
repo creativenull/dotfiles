399d9e735abdd4dec013e78465a1a4e9d702dcde
Arnold Chand <creativenull@outlook.com>
2023-01-20 13:48:33 -0400

    feat(wezterm): custom colors

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -23,11 +23,84 @@ return {
     bottom = 0,
     left = 0,
   },
-  colors = {
-    cursor_bg = '#e5e5e5',
-    cursor_fg = '#171717',
-  },
+  -- colors = {
+  --   cursor_bg = '#e5e5e5',
+  --   cursor_fg = '#171717',
+  -- },
   window_frame = {
     font = wezterm.font('JetBrainsMono Nerd Font'),
+  },
+  colors = {
+    tab_bar = {
+      -- The color of the strip that goes along the top of the window
+      -- (does not apply when fancy tab bar is in use)
+      background = '#0b0022',
+
+      -- The active tab is the one that has focus in the window
+      active_tab = {
+        -- The color of the background area for the tab
+        bg_color = '#1e40af',
+        -- The color of the text for the tab
+        fg_color = '#eff6ff',
+
+        -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+        -- label shown for this tab.
+        -- The default is "Normal"
+        intensity = 'Normal',
+
+        -- Specify whether you want "None", "Single" or "Double" underline for
+        -- label shown for this tab.
+        -- The default is "None"
+        underline = 'None',
+
+        -- Specify whether you want the text to be italic (true) or not (false)
+        -- for this tab.  The default is false.
+        italic = false,
+
+        -- Specify whether you want the text to be rendered with strikethrough (true)
+        -- or not for this tab.  The default is false.
+        strikethrough = false,
+      },
+
+      -- Inactive tabs are the tabs that do not have focus
+      inactive_tab = {
+        bg_color = '#1b1032',
+        fg_color = '#808080',
+
+        -- The same options that were listed under the `active_tab` section above
+        -- can also be used for `inactive_tab`.
+      },
+
+      -- You can configure some alternate styling when the mouse pointer
+      -- moves over inactive tabs
+      inactive_tab_hover = {
+        bg_color = '#1e40af',
+        fg_color = '#eff6ff',
+        italic = true,
+
+        -- The same options that were listed under the `active_tab` section above
+        -- can also be used for `inactive_tab_hover`.
+      },
+
+      -- The new tab button that let you create new tabs
+      new_tab = {
+        bg_color = '#1b1032',
+        fg_color = '#808080',
+
+        -- The same options that were listed under the `active_tab` section above
+        -- can also be used for `new_tab`.
+      },
+
+      -- You can configure some alternate styling when the mouse pointer
+      -- moves over the new tab button
+      new_tab_hover = {
+        bg_color = '#3b3052',
+        fg_color = '#909090',
+        italic = true,
+
+        -- The same options that were listed under the `active_tab` section above
+        -- can also be used for `new_tab_hover`.
+      },
+    },
   },
 }
