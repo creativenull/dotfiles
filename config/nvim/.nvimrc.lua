c4a6d7e0f968fd352c74d9a8295ee907d8e98bb8
Arnold Chand <creativenull@outlook.com>
2023-02-05 14:36:25 -0400

    fix(nvim): adjust runtime libs luals

--- config/nvim/.nvimrc.lua
+++ config/nvim/.nvimrc.lua
@@ -29,7 +29,10 @@ local function setup()
         },
         diagnostics = { globals = { 'vim' } },
         workspace = {
-          library = vim.api.nvim_get_runtime_file('', true),
+          library = {
+            [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+            [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+          },
           checkThirdParty = false,
         },
         telemetry = { enable = false },
