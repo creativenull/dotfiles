b0395788bd05b638e7512541cc8eefe790b2d270
Arnold Chand <creativenull@outlook.com>
2023-04-24 10:34:31 -0400

    fix(nvim): cleanup unneeded files

--- config/nvim/lua/user/utils.lua
+++ config/nvim/lua/user/utils.lua
@@ -81,4 +81,37 @@ function M.resize_win_vert(amount)
   end
 end
 
+---Set the highlights for transparent background,
+---to be used inside an autocmd
+---@return nil
+function M.set_transparent_bg()
+  -- Core highlights to make transparent
+  vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE' })
+  vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'NONE' })
+  vim.api.nvim_set_hl(0, 'LineNr', { bg = 'NONE', fg = '#888888' })
+  vim.api.nvim_set_hl(0, 'CursorLineNr', { bg = 'NONE' })
+  vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'NONE' })
+  vim.api.nvim_set_hl(0, 'Visual', { bg = '#555555' })
+
+  -- Sometimes comments are too dark, affects in tranparent mode
+  vim.api.nvim_set_hl(0, 'Comment', { fg = '#888888' })
+
+  -- Tabline
+  vim.api.nvim_set_hl(0, 'TabLineFill', { bg = 'NONE' })
+  vim.api.nvim_set_hl(0, 'TabLine', { bg = 'NONE' })
+
+  -- Float Border
+  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE' })
+  vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'NONE', fg = '#eeeeee' })
+
+  -- Vertical Line
+  vim.api.nvim_set_hl(0, 'ColorColumn', { bg = '#999999' })
+
+  -- LSP Diagnostics
+  vim.api.nvim_set_hl(0, 'ErrorFloat', { bg = 'NONE' })
+  vim.api.nvim_set_hl(0, 'WarningFloat', { bg = 'NONE' })
+  vim.api.nvim_set_hl(0, 'InfoFloat', { bg = 'NONE' })
+  vim.api.nvim_set_hl(0, 'HintFloat', { bg = 'NONE' })
+end
+
 return M
