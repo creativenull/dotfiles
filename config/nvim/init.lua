51c5c3dbf5f66006c4e7f575ef3e74a325486e43
Arnold Chand <creativenull@outlook.com>
2023-05-31 10:04:55 +0800

    fix(nvim): back to vim-fugitive

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -402,32 +402,7 @@ vim.api.nvim_create_autocmd('ColorScheme', {
 
 -- gin.vim Config
 -- ---
-local gin = {}
-
--- Push from git repo, notify user since this is async
-gin.push_origin = function()
-  -- local branch = vim.call('gitbranch#name')
-  local branch = vim.g.gitsigns_head
-  local cmd = string.format('Gin push origin %s', branch)
-  print(cmd)
-  vim.cmd(cmd)
-end
-
--- Pull from git repo, notify user since this is async
-gin.pull_origin = function()
-  -- local branch = vim.call('gitbranch#name')
-  local branch = vim.g.gitsigns_head
-  local cmd = string.format('Gin pull origin %s', branch)
-  print(cmd)
-  vim.cmd(cmd)
-end
-
-vim.keymap.set('n', '<Leader>gs', '<Cmd>GinStatus<CR>')
-vim.keymap.set('n', '<Leader>gp', gin.push_origin, { desc = 'Git push to origin from default branch' })
-vim.keymap.set('n', '<Leader>gpp', ':Gin push origin ')
-vim.keymap.set('n', '<Leader>gl', gin.pull_origin, { desc = 'Git pull from origin from default branch' })
-vim.keymap.set('n', '<Leader>gll', ':Gin pull origin ')
-vim.keymap.set('n', '<Leader>gb', '<Cmd>GinBranch<CR>')
+-- require('user.gin').setup()
 
 -- vim-json Config
 -- ---
@@ -543,8 +518,8 @@ Plug('gelguy/wilder.nvim')
 
 -- Git
 -- ---
-Plug('lambdalisue/gin.vim')
 Plug('lewis6991/gitsigns.nvim')
+Plug('tpope/vim-fugitive')
 
 -- UI/Aesthetics
 -- ---
