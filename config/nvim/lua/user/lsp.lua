e357d518b63392e923b3bc40bc2d7f30155553e2
Arnold Chand <creativenull@outlook.com>
2023-09-24 23:55:54 -0400

    fix(nvim): check for buf before fmt

--- config/nvim/lua/user/lsp.lua
+++ config/nvim/lua/user/lsp.lua
@@ -4,8 +4,8 @@ local function register_format_on_save()
   local lsp_group = vim.api.nvim_create_augroup('LspFormattingGroup', {})
   vim.api.nvim_create_autocmd('BufWritePost', {
     group = lsp_group,
-    callback = function()
-      local efm = vim.lsp.get_active_clients({ name = 'efm' })
+    callback = function(ev)
+      local efm = vim.lsp.get_active_clients({ name = 'efm', bufnr = ev.buf })
 
       if vim.tbl_isempty(efm) then
         return
