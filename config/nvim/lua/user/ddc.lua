bba675eb1816d72c3ddd11e06c95915713861806
Arnold Chand <creativenull@outlook.com>
2024-03-21 09:09:12 -0400

    fix(nvim): remove print debug

--- config/nvim/lua/user/ddc.lua
+++ config/nvim/lua/user/ddc.lua
@@ -12,8 +12,6 @@ local function autoimport_nvim_lsp(buf)
   local completeditem = vim.api.nvim_get_var('pum#completed_item')
   local active_clients = vim.lsp.get_active_clients({ bufnr = buf })
 
-  vim.print(completeditem)
-
   if completeditem.__sourceName == 'lsp' then
     for _, client in pairs(active_clients) do
       if
