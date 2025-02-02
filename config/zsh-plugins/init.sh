ae77d8b6ee73997c6c275af95794d75058e29eb2
Arnold Chand <creativenull@outlook.com>
2025-02-02 12:27:07 -0400

    fix(zsh): update init.sh

--- config/zsh-plugins/init.sh
+++ config/zsh-plugins/init.sh
@@ -1,11 +1,4 @@
 git remote add zsh-artisan https://github.com/jessarcher/zsh-artisan
-git subtree add --prefix=config/zsh-plugins/zsh-artisan zsh-artisan master --squash
-
 git remote add zsh-autosuggestions https://github.com/zsh-users/zsh-autosuggestions
-git subtree add --prefix=config/zsh-plugins/zsh-autosuggestions zsh-autosuggestions master --squash
-
 git remote add zsh-syntax-highlighting https://github.com/zsh-users/zsh-syntax-highlighting
-git subtree add --prefix=config/zsh-plugins/zsh-syntax-highlighting zsh-syntax-highlighting master --squash
-
 git remote add zsh-history-substring-search https://github.com/zsh-users/zsh-history-substring-search
-git subtree add --prefix=config/zsh-plugins/zsh-history-substring-search zsh-history-substring-search master --squash
