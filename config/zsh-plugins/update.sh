ca3de48b07e88011480dc8370e2f7cc897d0a976
Arnold Chand <creativenull@outlook.com>
2025-02-02 12:15:21 -0400

    feat(zsh): add update script

---
+++ config/zsh-plugins/update.sh
@@ -0,0 +1,11 @@
+git fetch zsh-artisan
+git subtree pull --prefix=config/zsh-plugins/zsh-artisan zsh-artisan master --squash
+
+git fetch zsh-autosuggestions
+git subtree pull --prefix=config/zsh-plugins/zsh-autosuggestions zsh-autosuggestions master --squash
+
+git fetch zsh-syntax-highlighting
+git subtree pull --prefix=config/zsh-plugins/zsh-syntax-highlighting zsh-syntax-highlighting master --squash
+
+git fetch zsh-history-substring-search
+git subtree pull --prefix=config/zsh-plugins/zsh-history-substring-search zsh-history-substring-search master --squash
