0dfd62bb65b4b5878587fb80ed6becf0e7950e9c
Arnold Chand <creativenull@outlook.com>
2025-02-10 00:00:49 -0400

    fix(scripts): ensure dir exists

--- scripts/ubuntu/1-install.sh
+++ scripts/ubuntu/1-install.sh
@@ -47,6 +47,7 @@ rm ~/.local/bin/asdf.tar.gz
 echo 'Installing kitty'
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+mkdir -p ~/.local/share/applications
 cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
 cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
 sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
