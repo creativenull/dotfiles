175ed44e0c980f2ede01a6a533a109caa15ea930
Arnold Chand <creativenull@outlook.com>
2025-02-10 00:22:46 -0400

    fix(script): efm

--- scripts/ubuntu/1-install.sh
+++ scripts/ubuntu/1-install.sh
@@ -81,10 +81,13 @@ echo 'Installing starship'
 curl -sS https://starship.rs/install.sh | sh -s -- -b ~/.local/bin
 
 echo 'Installing efm-langserver'
-EFM_URL="https://github.com/mattn/efm-langserver/releases/download/v0.0.54/efm-langserver_v0.0.54_linux_amd64.tar.gz"
+EFM_VER="v0.0.54"
+EFM_URL="https://github.com/mattn/efm-langserver/releases/download/$EFM_VER/efm-langserver_${EFM_VER}_linux_amd64.tar.gz"
 wget "$EFM_URL" -O ~/.local/bin/efm-langserver.tar.gz
 tar -xzf ~/.local/bin/efm-langserver.tar.gz -C ~/.local/bin
 rm ~/.local/bin/efm-langserver.tar.gz
+mv ~/.local/bin/efm-langserver_${EFM_VER}_linux_amd64/efm-langserver ~/.local/bin
+rm -rf ~/.local/bin/efm-langserver_${EFM_VER}_linux_amd64
 
 echo 'Installing stylua'
 STYLUA_URL="https://github.com/JohnnyMorganz/StyLua/releases/download/v2.0.2/stylua-linux-x86_64.zip"
