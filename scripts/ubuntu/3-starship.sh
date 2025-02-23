8163a16d6181f7c2b0f370873e8361dbeac3f654
Arnold Chand <creativenull@outlook.com>
2025-02-22 20:53:40 -0400

    fix(scripts/starship): use releases url

--- scripts/ubuntu/3-starship.sh
+++ scripts/ubuntu/3-starship.sh
@@ -1,4 +1,8 @@
 echo '---'
 echo 'Installing starship'
 echo '---'
-curl -sS https://starship.rs/install.sh | sh -s -- -b ~/.local/bin
+STARSHIP_VER="v1.22.1"
+STARSHIP_URL="https://github.com/starship/starship/releases/download/${STARSHIP_VER}/starship-x86_64-unknown-linux-gnu.tar.gz"
+wget "$STARSHIP_URL" -O ~/.local/bin/starship.tar.gz
+tar -xzf ~/.local/bin/starship.tar.gz -C ~/.local/bin
+rm ~/.local/bin/starship.tar.gz
