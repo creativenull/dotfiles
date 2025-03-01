246a41e3c49eaef73d1dd17a56dc5ebfad93f14b
Arnold Chand <creativenull@outlook.com>
2025-03-01 15:14:54 -0400

    fix(scripts): lsd url

--- scripts/ubuntu/5-lsd.sh
+++ scripts/ubuntu/5-lsd.sh
@@ -5,6 +5,6 @@ echo 'Installing lsd'
 echo '---'
 LSD_VER="1.1.5"
 LSD_URL="https://github.com/lsd-rs/lsd/releases/download/v${LSD_VER}/lsd_${LSD_VER}_amd64.deb"
-wget "$STYLUA_URL" -O ~/.local/bin/lsd.deb
+wget "$LSD_URL" -O ~/.local/bin/lsd.deb
 sudo dpkg -i ~/.local/bin/lsd.deb
 rm -f ~/.local/bin/lsd.deb
