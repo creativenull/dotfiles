ec170d74b5b7fa41f043f31c24bf6a57255f47bd
Arnold Chand <creativenull@outlook.com>
2025-03-01 14:36:27 -0400

    fix(scripts): copy to ~/.local/share/fonts

--- scripts/ubuntu/9-jetbrains.sh
+++ scripts/ubuntu/9-jetbrains.sh
@@ -1,8 +1,8 @@
 echo '---'
 echo 'Installing jetbrains mono nerdfonts'
 echo '---'
-mkdir -p ~/.fonts
+mkdir -p ~/.local/share/fonts
 JETBRAINS_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip"
-wget "$JETBRAINS_URL" -O ~/.fonts/JetBrainsMono.zip
-unzip ~/.fonts/JetBrainsMono.zip -d ~/.fonts
-rm ~/.fonts/JetBrainsMono.zip
+wget "$JETBRAINS_URL" -O ~/.local/share/fonts/JetBrainsMono.zip
+unzip ~/.local/share/fonts/JetBrainsMono.zip -d ~/.local/share/fonts
+rm ~/.local/share/fonts/JetBrainsMono.zip
