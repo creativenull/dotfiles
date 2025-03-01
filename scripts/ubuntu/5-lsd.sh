fefa65b8b65ffe7bf3cbf86181115f85faf87f13
Arnold Chand <creativenull@outlook.com>
2025-03-01 14:39:04 -0400

    feat(scripts): add lsd

--- scripts/ubuntu/5-lsd.sh
+++ scripts/ubuntu/5-lsd.sh
@@ -1,7 +1,8 @@
 echo '---'
-echo 'Installing stylua'
+echo 'Installing lsd'
 echo '---'
-STYLUA_URL="https://github.com/JohnnyMorganz/StyLua/releases/download/v2.0.2/stylua-linux-x86_64.zip"
-wget "$STYLUA_URL" -O ~/.local/bin/stylua.zip
-unzip ~/.local/bin/stylua.zip -d ~/.local/bin
-rm ~/.local/bin/stylua.zip
+LSD_VER="1.1.5"
+LSD_URL="https://github.com/lsd-rs/lsd/releases/download/v${LSD_VER}/lsd_${LSD_VER}_amd64.deb"
+wget "$STYLUA_URL" -O ~/.local/bin/lsd.deb
+sudo dpkg -i ~/.local/bin/lsd.deb
+rm -f ~/.local/bin/lsd.deb
