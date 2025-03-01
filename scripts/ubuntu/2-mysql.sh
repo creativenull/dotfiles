5bdf96d85da77e99a79a1c3898b431707d1b3514
Arnold Chand <creativenull@outlook.com>
2025-03-01 14:42:35 -0400

    fix(scripts): mysql-server install, only need user

--- scripts/ubuntu/2-mysql.sh
+++ scripts/ubuntu/2-mysql.sh
@@ -1,8 +1,21 @@
 echo '---'
 echo 'Install mysql-server'
 echo '---'
-MYSQL_PWD=password
-echo "mysql-server mysql-server/root_password password $MYSQL_PWD" | debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password $MYSQL_PWD" | debconf-set-selections
-echo "mysql-server mysql-server/start_on_boot boolean true" | debconf-set-selections
 sudo apt-get install -y mysql-server
+
+# Secure the MySQL installation (remove anonymous users, disallow remote root login, etc.)
+mysql_secure_installation <<EOF
+
+n # Change the password? No
+n # Remove anonymous users? Yes
+y # Disallow root login remotely? Yes
+y # Remove test database and access to it? Yes
+y # Reload privilege tables now? Yes
+EOF
+
+# Login to MySQL and create the user and grant privileges
+sudo mysql <<EOF
+CREATE USER 'creativenull'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'creativenull'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
