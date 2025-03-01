0fd741b4868903b6b8feb61b66ba520657b95904
Arnold Chand <creativenull@outlook.com>
2025-03-01 15:37:39 -0400

    feat(scripts): libsodium dep

--- scripts/ubuntu/4-asdf.sh
+++ scripts/ubuntu/4-asdf.sh
@@ -32,7 +32,7 @@ echo '---'
 echo 'Installing php'
 echo '---'
 PHP_VER="8.4.4"
-sudo apt-get install -y autoconf bison build-essential curl gettext git libgd-dev libcurl4-openssl-dev libedit-dev libicu-dev libjpeg-dev libmysqlclient-dev libonig-dev libpng-dev libpq-dev libreadline-dev libsqlite3-dev libssl-dev libxml2-dev libzip-dev openssl pkg-config re2c zlib1g-dev
+sudo apt-get install -y autoconf bison build-essential curl gettext git libgd-dev libcurl4-openssl-dev libedit-dev libicu-dev libjpeg-dev libmysqlclient-dev libonig-dev libpng-dev libpq-dev libreadline-dev libsqlite3-dev libssl-dev libxml2-dev libzip-dev openssl pkg-config re2c zlib1g-dev libsodium-dev
 asdf plugin add php
 ASDF_CONCURRENCY=4 PHP_CONFIGURE_OPTIONS="--with-openssl --with-curl --with-zlib --with-readline --with-gettext --with-sodium" asdf install php $PHP_VER
 asdf set -u php $PHP_VER
