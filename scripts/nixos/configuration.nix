0fe2477073014527664e4c1cc57a6916b7246d09
Arnold Chand <creativenull@outlook.com>
2025-05-04 01:23:46 -0400

    fix(nixos): cleanup

--- scripts/nixos/configuration.nix
+++ scripts/nixos/configuration.nix
@@ -1,14 +1,7 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-    ];
+  imports = [ ./hardware-configuration.nix ];
 
   nix.gc.automatic = true;
   nix.gc.dates = "weekly";
