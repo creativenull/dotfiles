b09f71e9feddb54eb2a71bb97c8d44a454b04e8b
Arnold Chand <creativenull@outlook.com>
2025-05-06 23:53:22 -0400

    fix(ubuntu): cleanup echos

--- scripts/ubuntu/setup/00-init.sh
+++ scripts/ubuntu/setup/00-init.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
-echo '---'
 echo 'Creating ~/.local/bin and add to $PATH'
-echo '---'
 
 mkdir -p ~/.local/bin
 export PATH="$HOME/.local/bin:$PATH"
 
-echo '---'
 echo 'System update'
-echo '---'
 
 sudo apt update && sudo apt upgrade -y
 sudo snap refresh
