9db93a28cb830eb0da0d4e7d7277a4d1f8a5c903
Arnold Chand <creativenull@outlook.com>
2025-05-10 16:04:32 -0400

    feat(ubuntu): do not go to sleep if installing

--- scripts/ubuntu/setup.sh
+++ scripts/ubuntu/setup.sh
@@ -5,6 +5,14 @@ echo '---'
 echo 'Starting setup script'
 echo '---'
 
+# From omakub/install.sh
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.session idle-delay 0
+
 for script in $(ls $HOME/dotfiles/scripts/ubuntu/setup/*.sh); do
 	sh "$script"
 done
+
+# From omakub/install.sh
+gsettings set org.gnome.desktop.screensaver lock-enabled true
+gsettings set org.gnome.desktop.session idle-delay 300
