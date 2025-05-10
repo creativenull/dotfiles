4078c7e5196e74e7458124a875ef63cb77ca1213
Arnold Chand <creativenull@outlook.com>
2025-05-10 09:58:45 -0400

    fix(nvim): adjust keybindings

--- config/nvim/lua/user/codeium.lua
+++ config/nvim/lua/user/codeium.lua
@@ -6,14 +6,14 @@ function M.setup()
     virtual_text = {
       enabled = true,
       filetypes = { ["fern-replacer"] = false },
-      map_keys = false,
+      map_keys = true,
       key_bindings = {
         accept = "<Tab>",
         accept_word = false,
         accept_line = false,
-        next = "<C-]>",
-        prev = "<C-[>",
-        clear = "<C-x>",
+        next = "<M-]>",
+        prev = "<M-[>",
+        clear = false,
       },
     },
   })
