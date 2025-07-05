a3e66d28b4f9ab3bd015a677f138400be09e1ea9
Arnold Chand <creativenull@outlook.com>
2025-07-05 10:16:34 -0400

    fix(hammerspoon/paperwm): smaller gap

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -5,7 +5,7 @@ PaperWM.window_filter:rejectApp("Picture in Picture")
 PaperWM.window_filter:rejectApp("qemu-system-aarch64")
 PaperWM.window_filter:setScreens({ "Built%-in Retina Display" })
 
-PaperWM.window_gap = 14
+PaperWM.window_gap = 10
 PaperWM.window_ratios = { 0.38195, 0.5, 0.61804 }
 
 PaperWM:bindHotkeys(PaperWM.default_hotkeys)
