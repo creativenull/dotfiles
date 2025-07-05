f8488172162a3476e5f0c2013d43dc72d96856d7
Arnold Chand <creativenull@outlook.com>
2025-07-05 10:15:04 -0400

    fix(hammerspoon/paperwm): rm dups, filter on top

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,7 +1,10 @@
 PaperWM = hs.loadSpoon("PaperWM")
 
 PaperWM.window_filter:rejectApp("Finder")
+PaperWM.window_filter:rejectApp("Picture in Picture")
 PaperWM.window_filter:rejectApp("qemu-system-aarch64")
+PaperWM.window_filter:setScreens({ "Built%-in Retina Display" })
+
 PaperWM.window_gap = 14
 PaperWM.window_ratios = { 0.38195, 0.5, 0.61804 }
 
@@ -11,9 +14,5 @@ hs.hotkey.bind({ "alt", "cmd" }, ",", PaperWM.actions.focus_left)
 hs.hotkey.bind({ "alt", "cmd" }, ".", PaperWM.actions.focus_right)
 hs.hotkey.bind({ "alt", "cmd", "shift" }, ",", PaperWM.actions.swap_left)
 hs.hotkey.bind({ "alt", "cmd", "shift" }, ".", PaperWM.actions.swap_right)
-
-PaperWM.window_filter:rejectApp("Finder")
-PaperWM.window_filter:rejectApp("Picture in Picture")
-PaperWM.window_filter:setScreens({ "Built%-in Retina Display" })
 
 PaperWM:start()
