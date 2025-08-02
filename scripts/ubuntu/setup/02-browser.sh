11fab7acc3f7aa5518bf5c816cdbaad9db565ec0
Arnold Chand <creativenull@outlook.com>
2025-08-02 09:34:18 -0400

    fix(ubuntu): mkdir builds

--- scripts/ubuntu/setup/02-browser.sh
+++ scripts/ubuntu/setup/02-browser.sh
@@ -5,5 +5,8 @@ echo '=> Installing vivaldi'
 VIVALDI_VER="7.5.3735.58-1"
 VIVALDI_URL="https://downloads.vivaldi.com/stable/vivaldi-stable_${VIVALDI_VER}_amd64.deb"
 
+mkdir -p ~/.builds
 wget "$VIVALDI_URL" -O ~/.builds/vivaldi.deb
 sudo dpkg -i ~/.builds/vivaldi.deb
+
+rm -rf ~/.builds/vivaldi.deb
