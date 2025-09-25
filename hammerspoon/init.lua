cf770c7b96465e588c2bb395eca8f9caf83ab6bf
Arnold Chand <creativenull@outlook.com>
2025-09-25 10:13:17 -0400

    fix(paperwm): grab actions from actions()

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -10,9 +10,10 @@ PaperWM.window_ratios = { 0.38195, 0.5, 0.61804 }
 
 PaperWM:bindHotkeys(PaperWM.default_hotkeys)
 
-hs.hotkey.bind({ "alt", "cmd" }, ",", PaperWM.actions.focus_left)
-hs.hotkey.bind({ "alt", "cmd" }, ".", PaperWM.actions.focus_right)
-hs.hotkey.bind({ "alt", "cmd", "shift" }, ",", PaperWM.actions.swap_left)
-hs.hotkey.bind({ "alt", "cmd", "shift" }, ".", PaperWM.actions.swap_right)
+local actions = PaperWM.actions.actions()
+hs.hotkey.bind({ "alt", "cmd" }, ",", actions.focus_left)
+hs.hotkey.bind({ "alt", "cmd" }, ".", actions.focus_right)
+hs.hotkey.bind({ "alt", "cmd", "shift" }, ",", actions.swap_left)
+hs.hotkey.bind({ "alt", "cmd", "shift" }, ".", actions.swap_right)
 
 PaperWM:start()
