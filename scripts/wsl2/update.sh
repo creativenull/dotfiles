8fa4b7c58438a1d125e414918f6298817b1cb4c4
Arnold Chand <creativenull@outlook.com>
2026-07-15 10:57:57 -0400

    fix(wsl2/update): no twice confirmation on brew

--- scripts/wsl2/update.sh
+++ scripts/wsl2/update.sh
@@ -13,7 +13,7 @@ brew update && brew outdated
 
 read -p "=> Run brew upgrade? (y/N) " brew_choice
 if [ "$brew_choice" = "y" ]; then
-    brew upgrade
+    brew upgrade --yes
 fi
 
 printf "=> Checking for outdated npm packages\n"
